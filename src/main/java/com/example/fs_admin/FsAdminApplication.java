afc787c328e6584b44a9de02699fcc8fc80a8042
chenziwen <1954635917@qq.com>
2024-08-24 16:56:28 +0800

    init

---
+++ src/main/java/com/example/fs_admin/FsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.fs_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsAdminApplication.class, args);
+	}
+
+}
